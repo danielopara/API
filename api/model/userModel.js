ac2c29139cee50bb2349058e8ea00618192dc3c5
Daniel Opara <oparadaniv@gmail.com>
2023-07-08 16:05:30 +0100

    background image & profile image

--- api/model/userModel.js
+++ api/model/userModel.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose')
 const userSchema = mongoose.Schema({
     _id : mongoose.Schema.Types.ObjectId,
     displayName : {type : String, required : true},
-    description : {type : String, required : true}
+    description : {type : String, required : true},
+    userImage : {type : String },
+    backgroundImage : {type : String}
 })
 
 module.exports = mongoose.model("User", userSchema)
--- api/user.js
+++ api/user.js
@@ -2,10 +2,40 @@ const express = require('express')
 const mongoose = require('mongoose')
 const router = express.Router()
 const User = require('./model/userModel')
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination : function (req, file, cb){
+        cb(null, './profileImage/')
+    },
+    filename : function (req, file, cb){
+        cb(null, file.originalname)
+    }
+})
+
+const storage2 = multer.diskStorage({
+    destination : function (req, file, cb){
+        cb(null, './backgroundImage/')
+    },
+    filename : function (req, file, cb){
+        cb(null, file.originalname)
+    }
+})
+
+const fileFilter = (req, file, cb) =>{
+    if(file.mimetype === 'image/jpeg' || file.mimetype == 'image/png'){
+    cb(null, true)
+    } else{
+    cb(null, false);
+    console.log('not an image')
+    }
+}
+const profileImage = multer({storage : storage, fileFilter : fileFilter})
+const backgroundImage = multer({storage : storage2, fileFilter : fileFilter})
 
 router.get('/', (req, res, next)=>{
     User.find()
-    .select('_id displayName description')
+    .select('_id displayName description userImage backgroundImage')
     .exec()
     .then(doc=>{
         const response = {
@@ -15,6 +45,8 @@ router.get('/', (req, res, next)=>{
                     _id : doc._id,
                     displayName : doc.displayName,
                     description : doc.description,
+                    userImage : doc.userImage,
+                    backgroundImage : doc.backgroundImage,
                     details : {
                         type : 'GET',
                         about : 'http://localhost:2020/users/' + doc._id
@@ -36,7 +68,7 @@ router.post('/', (req,res,next)=>{
     const user = new User({
         _id : new mongoose.Types.ObjectId(),
         displayName : req.body.displayName,
-        description : req.body.description
+        description : req.body.description,
     })
     user.save().then(result=>{
         console.log(result)
@@ -46,6 +78,8 @@ router.post('/', (req,res,next)=>{
                 _id : result._id,
                 displayName : result.displayName,
                 description : result.description,
+                userImage : result.userImage,
+                backgroundImage : result.backgroundImage,
                 moreUsers : {
                     type : 'GET',
                     url : 'http://localhost:2020/users'
@@ -59,6 +93,68 @@ router.post('/', (req,res,next)=>{
     })
 })
 
+router.post('/:userId/profileImage', profileImage.single('userImage'), async (req, res, next)=>{
+    const id = req.params.userId
+    const userImage = req.file
+   
+    try{
+        const user = await User.findById(id);
+
+        if (!user) {
+          return res.status(404).send('User not found.');
+        }
+        user.userImage = userImage.filename;
+    
+        await user.save().then(result=>{
+            console.log(result)
+            res.status(200).json({
+                message : "added profile image",
+                user : {
+                    id : result._id,
+                    displayName : result.displayName,
+                    description : result.description,
+                    userImage : result.userImage,
+                    backgroundImage : result.backgroundImage,
+                }
+            });
+        });
+    } catch(err){
+        console.error('Error:', err);
+        res.status(500).send('Error updating user image.');
+    }
+})
+
+router.post('/:userId/backgroundImage', backgroundImage.single('backgroundImage'), async (req, res, next)=>{
+    const id = req.params.userId
+    const backgroundImage = req.file
+   
+    try{
+        const user = await User.findById(id);
+
+        if (!user) {
+          return res.status(404).send('User not found.');
+        }
+        user.backgroundImage = backgroundImage.filename;
+    
+        await user.save().then(result=>{
+            console.log(result)
+            res.status(200).json({
+                message : "added background image",
+                user : {
+                    id : result._id,
+                    displayName : result.displayName,
+                    description : result.description,
+                    userImage : result.userImage,
+                    backgroundImage : result.backgroundImage,
+                }
+            });
+        });
+    } catch(err){
+        console.error('Error:', err);
+        res.status(500).send('Error updating user image.');
+    }
+})
+
 router.get('/:userId', (req, res, next)=>{
     const id = req.params.userId
    User.findById(id)
@@ -69,6 +165,7 @@ router.get('/:userId', (req, res, next)=>{
             _id : doc._id,
             displayName : doc.displayName,
             description : doc.description,
+            userImage : doc.userImage,
             moreUsers : {
                 type : 'GET',
                 url  : 'http://localhost:2020'
